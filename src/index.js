b9f6d1a2968d11f3ff5067ae5fd734e4f949b1a5
lvshijie <lvshijie@aiwending.com>
2018-12-27 09:20:23 +0800

    up

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//const puppeteer = require('puppeteer')
+
+const path = require('path')
+const p = path.resolve(__dirname,'./a')
+const p2 = path.join(__dirname, '/a')
+// (async () => {
+//   const browser = await puppeteer.launch()
+//   const page = await browser.newPage()
+//   await page.goto('https://www.baidu.com')
+//   await page.screenshot({
+//     path: ''
+//   })
+// })()
+///start node.js/10-2/7:11
